ef5973d4cc5ed4bba9cf76269e8c19b773a35eeb
ienuoyibo <ienuoyibo@interac.ca>
2024-11-29 00:15:42 -0500

    Updating variables

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket"
-    key            = "aurora-database/root-state"
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Hackathon-ify"
+
+    workspaces {
+      name = "hackathon-aurora-db"
+    }
   }
 }
