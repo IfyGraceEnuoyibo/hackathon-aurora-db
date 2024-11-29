c8fc7f631822f5e91601ef75855f2fcb1031d605
ienuoyibo <ienuoyibo@interac.ca>
2024-11-29 00:56:30 -0500

    Updating output

--- modules/aurora/outputs.tf
+++ modules/aurora/outputs.tf
@@ -3,7 +3,12 @@ output "aurora_endpoint" {
   value       = aws_rds_cluster.aurora.endpoint
 }
 
-output "aurora_writer_instance" {
-  description = "The writer instance endpoint of the Aurora cluster"
-  value       = aws_rds_cluster_instance.aurora_instance[0].address
+output "aurora_reader_endpoint" {
+  description = "The reader endpoint of the Aurora cluster (for read operations)"
+  value       = aws_rds_cluster.aurora.reader_endpoint
+}
+
+output "aurora_instance_endpoints" {
+  description = "The endpoints of the Aurora instances (for debugging or direct connection)"
+  value       = [for instance in aws_rds_cluster_instance.aurora_instance : instance.endpoint]
 }
